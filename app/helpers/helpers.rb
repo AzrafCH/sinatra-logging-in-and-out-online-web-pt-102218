b2668f0545b8e5f237b1b62f70d4c11ac1221a22
Azraf Chowdhury <achowdhur@ltu.edu>
2019-02-17 20:50:48 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,9 @@
 class Helpers
 
-  def helers.current_user(session)
+  def self.current_user(session)
     @user = user.find_by(id: session[:user_id])
   end
+
+  def self.is_logged_in?(session)
+    !!session[:user_id]
 end
