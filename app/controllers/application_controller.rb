3f9a7775440aed2cc81d2c472a0e9595ab4228c7
Azraf Chowdhury <achowdhur@ltu.edu>
2019-02-17 21:44:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params["username"], password: params["password"])
-    if Helpers.current_user(session)
+    if Helpers.current_user(session) != nil && @users.password == params["password"]
       session[:user_id] = @user.id
       redirect "/account"
     else
